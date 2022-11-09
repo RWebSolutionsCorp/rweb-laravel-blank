bca3056308f3f4ebe2372f98c9c0751e17de0e93
RWebSolutionsCorp <cloud.rweb@gmail.com>
2022-11-09 08:36:19 +0800

    LTS

--- webpack.mix.js
+++ webpack.mix.js
@@ -15,14 +15,16 @@ mix
 
 // FRONT ASSETS
 .scripts([
+    'node_modules/jquery/dist/jquery.min.js',
     'node_modules/bootstrap/dist/js/bootstrap.bundle.min.js',
-    // 'node_modules/bootstrap/dist/js/bootstrap.min.js',
-    'node_modules/@fortawesome/fontawesome-free/css/all.css',
+    'node_modules/owl.carousel/dist/owl.carousel.min.js',
     'resources/js/global.js'
 ], 'public/js/app.js')
 
 .styles([
     'node_modules/bootstrap/dist/css/bootstrap.min.css',
+    'node_modules/@fortawesome/fontawesome-free/css/all.css',
+    'node_modules/owl.carousel/dist/assets/owl.carousel.min.css',
     'resources/css/global.css'
 ], 'public/css/app.css')
 
@@ -30,7 +32,10 @@ mix
 .styles(
     [
         'node_modules/admin-lte/plugins/fontawesome-free/css/all.min.css',
+        'node_modules/admin-lte/plugins/select2/css/select2.min.css',
+        'node_modules/admin-lte/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css',
         'node_modules/admin-lte/dist/css/adminlte.min.css',
+
     ],
     'public/css/admin.css'
 )
@@ -39,11 +44,19 @@ mix
     [
         'node_modules/admin-lte/plugins/jquery/jquery.min.js',
         'node_modules/admin-lte/plugins/bootstrap/js/bootstrap.bundle.min.js',
+        'node_modules/admin-lte/plugins/select2/js/select2.full.min.js',
+        'node_modules/admin-lte/plugins/bs-custom-file-input/bs-custom-file-input.min.js',
         'node_modules/admin-lte/dist/js/adminlte.min.js'
     ],
     'public/js/admin.js'
 )
 
 .copyDirectory('node_modules/@fortawesome/fontawesome-free/webfonts', 'public/webfonts')
+
+.copy('node_modules/bootstrap/dist/css/bootstrap.min.css.map', 'public/css')
+.copy('node_modules/bootstrap/dist/js/bootstrap.bundle.min.js.map', 'public/js')
+
+.copy('node_modules/admin-lte/dist/css/adminlte.min.css.map', 'public/css')
+.copy('node_modules/admin-lte/dist/js/adminlte.min.js.map', 'public/js')
 
 ;
